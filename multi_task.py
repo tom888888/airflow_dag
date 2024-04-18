c5c620f9bc35087ab5dc98ed2057c8c3e09fc825
tom888888 <33222288+tom888888@users.noreply.github.com>
2024-04-18 17:19:53 +0800

    Update multi_task.py

--- multi_task.py
+++ multi_task.py
@@ -13,21 +13,24 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG(
-    'multi_task_dag',
-    default_args=default_args,
+with DAG(
+    dag_id = "multi_task_dag",
+    default_args = default_args,
+    description = '測試task上限',
     start_date=datetime(2024, 4, 18),
     schedule_interval=timedelta(days=1)
-)
-range_list=[1,2]
-with TaskGroup(group_id='multi_task') as multi_task:
+) as dag:
   
-  multi_task_list = [
-      BashOperator(
-          task_id=f"multi_task_{num}",
-          bash_command=test_task.py,
-          dag=dag)
-  for num in range_list]
-  multi_task_list
 
-multi_task
+    range_list=[1,2]
+    with TaskGroup(group_id='multi_task') as multi_task:
+    
+    multi_task_list = [
+        BashOperator(
+            task_id=f"multi_task_{num}",
+            bash_command=test_task.py,
+            dag=dag)
+    for num in range_list]
+    multi_task_list
+
+    multi_task
